5872c39e8d85bb20d345b4518f478afd63281296
Eugene Kasap <eugene.kasap@olx.com>
2025-07-18 16:27:14 +0200

    feat: add canonical URL handling in app component and clean up index.html

--- vitaliikasap/src/app/app.ts
+++ vitaliikasap/src/app/app.ts
@@ -1,16 +1,19 @@
 import {Component, inject, Inject, PLATFORM_ID} from '@angular/core';
-import { ThemeSwitcherComponent } from './shared/ui/components/theme-switcher/theme-switcher.component';
-import { RouterOutlet } from '@angular/router';
+import {ThemeSwitcherComponent} from './shared/ui/components/theme-switcher/theme-switcher.component';
+import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
 import {SideNavComponent} from './features/side-nav/components/side-nav/side-nav.component';
 import {LangSwitcherComponent} from './shared/ui/components/lang-switcher/lang-switcher.component';
 import {BurgerMenuComponent} from './features/burger-menu/components/burger-menu/burger-menu.component';
 import {CursorComponent} from './shared/ui/components/cursor/cursor.component';
 import {NetAnimationComponent} from './features/net-animation/components/net-animation.component';
 import {IntroFeatureService, IntroOverlayComponent} from './features/intro';
-import { OnInit } from '@angular/core';
+import {OnInit} from '@angular/core';
 import {DeviceDetectionService} from './shared/services/device-detection.service';
-import { isPlatformBrowser } from '@angular/common';
+import {isPlatformBrowser} from '@angular/common';
 import {MobileNavArrowsComponent} from './shared/ui/components/mobile-nav-arrows/mobile-nav-arrows.component';
+import {DOCUMENT} from '@angular/common';
+import {filter} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -22,7 +25,7 @@ import {MobileNavArrowsComponent} from './shared/ui/components/mobile-nav-arrows
     }
 
     <app-net-animation></app-net-animation>
-    <app-burger-menu class="show xl:hidden" />
+    <app-burger-menu class="show xl:hidden"/>
 
     @if (isDesktop && isBrowser) {
       <app-cursor></app-cursor>
@@ -47,10 +50,21 @@ export class App implements OnInit {
 
   constructor(
     private deviceService: DeviceDetectionService,
-    @Inject(PLATFORM_ID) private platformId: Object
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(DOCUMENT) private doc: Document,
+    private router: Router
   ) {
     this.isBrowser = isPlatformBrowser(this.platformId);
+
+    if (this.isBrowser) {
+      this.router.events
+        .pipe(filter(event => event instanceof NavigationEnd))
+        .subscribe(() => {
+          this.setCanonicalURL();
+        });
+    }
   }
+
 
   ngOnInit() {
     if (this.isBrowser) {
@@ -60,5 +74,15 @@ export class App implements OnInit {
 
   onIntroEnd() {
     this.intro.showIntro();
+  }
+
+  private setCanonicalURL() {
+    let link = this.doc.querySelector("link[rel='canonical']") as HTMLLinkElement | null;
+    if (!link) {
+      link = this.doc.createElement('link') as HTMLLinkElement;
+      link.rel = 'canonical';
+      this.doc.head.appendChild(link);
+    }
+    link.href = 'https://vitaliikasap.com' + location.pathname;
   }
 }
